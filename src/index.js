7cd7b081456ca66a79d5daa3cdc3b28de38e88fd
Rohama_Majeed <rohamamajeed3@gmail.com>
2024-06-30 11:00:46 +0500

    save file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import ConnectDB from "./db/index.js";
-import dotenv from "dotenv";
-dotenv.config({
-    path:'./env'
-})
+// import dotenv from "dotenv";
+// dotenv.config({
+//     path:'./env'
+// })
 ConnectDB();
 
 
