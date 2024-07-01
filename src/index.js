c01de4e3cc44e3cc4cb16b815784425eb175876f
Rohama_Majeed <rohamamajeed3@gmail.com>
2024-07-01 10:58:33 +0500

    api response and error handling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
+import { app } from "./app.js";
 import ConnectDB from "./db/index.js";
-// import dotenv from "dotenv";
-// dotenv.config({
-//     path:'./env'
-// })
-ConnectDB();
+import dotenv from "dotenv";
+dotenv.config({
+    path:'./env'
+})
+ConnectDB().then(()=>{
+    app.listen(process.env.PORT || 8000 ,() =>{
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+    console.log("DB Connected");
+}).catch((err)=>{
+    console.log("DB Connection Failed:",err);
+})
 
 
 
