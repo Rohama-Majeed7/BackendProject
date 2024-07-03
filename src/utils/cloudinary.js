405878fb982ec1a8415b6b24a84b0e5f79f64c01
Rohama_Majeed <rohamamajeed3@gmail.com>
2024-07-03 11:37:56 +0500

    cloudinary and multer set up

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET, // Click 'View Credentials' below to copy your API secret
+});
+
+const uploadOnCloudinary = async function (localPath) {
+  try {
+    if (!localPath) return null;
+    const result = await cloudinary.uploader.upload(localPath, {
+      resource_type: "auto",
+    });
+    console.log("flie is uploaded:", result.url);
+    return result;
+  } catch (error) {
+    fs.unlinkSync(localPath);
+    return null;
+  }
+};
+
+
+export {uploadOnCloudinary}
