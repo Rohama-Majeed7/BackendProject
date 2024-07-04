13e5f44ef2ed1dac7e6fe64a6518bdcc59e8436a
Rohama_Majeed <rohamamajeed3@gmail.com>
2024-07-04 11:45:23 +0500

    adding additional things

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
-import asyncHandler from "../utils/asyncHandler.js";
+// import asyncHandler from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req,res) =>{
+const registerUser =  (req,res) =>{
 return res.status(200).json({
-    message:"ok rohama majeed"
+    message:"hi"
 })
-})
+}
 
 export default registerUser
